4049a89d3132c69ccc103d9b58f2db2c820fdfae
Bilal Durnagöl <bilaldurnagol@gmail.com>
2023-04-04 17:19:13 +0300

    Initial Commit

---
+++ AnimatedStickyHeader/AnimatedStickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStickyHeaderApp.swift
+//  AnimatedStickyHeader
+//
+//  Created by Bilal Durnagöl on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
